b5f0a3ac03a710d234262f19525011323483d3a4
jotinhakdlk <kudlake.joao@escola.pr.gov.br>
2026-06-11 09:50:58 -0300

    Add files via upload

--- integração/app.py
+++ integração/app.py
@@ -4,9 +4,19 @@ from flask import Flask, render_template, request
 ## Conexão com sql e python
 import mysql.connector 
 
+app = Flask(__name__)
+
+## Cria conexão com MySQL
 bd_config = {
     'host': 'localhost',
     'user': 'root',
-    'password': 'escola',
+    'password': '',
     'database': 'cadastro1'
 }
+
+## Criação de rota para arquivo html principal
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/cadastro', methods=['POST'])
