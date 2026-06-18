9f6b51eb4f33ed09d9526fe527a67ddccc7fcf6f
jotinhakdlk <kudlake.joao@escola.pr.gov.br>
2026-06-18 09:51:59 -0300

    Add files via upload

--- integração/app.py
+++ integração/app.py
@@ -1,12 +1,16 @@
 ## Import do framework,
 ## render_templete para leitura do HTML e reqest para captura de dados
+
 from flask import Flask, render_template, request
-## Conexão com sql e python
+
+## Conexao com sql e python
+
 import mysql.connector 
 
 app = Flask(__name__)
 
-## Cria conexão com MySQL
+## Cria conexao com MySQL
+
 bd_config = {
     'host': 'localhost',
     'user': 'root',
@@ -14,9 +18,20 @@ bd_config = {
     'database': 'cadastro1'
 }
 
-## Criação de rota para arquivo html principal
+## Criacao de rota para arquivo html principal
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/cadastro', methods=['POST'])
+def criar_cadastro():
+    cpf = request.form['cpf']
+    primeiro_nome= request.form['primeiro_nome']
+    sobrenome = request.form['sobrenome']
+    idade = request.form['idade']
+
+    conexao = mysql.connector.connect(**bd_config)
+    curso = conexao.cursor()
+
+    query = "INSERT INTO cliente1 (CPF, PRIMEIRO_NOME, SOBRENOME, IDADE) VALUES (%s, %s, %s, %s)"
